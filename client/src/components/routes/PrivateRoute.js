36bad9d86ddeb55ec61ed22e5f85548e2ae191cf
Dar Dualba <ddualba@gmail.com>
2020-11-14 14:16:18 -0800

    Private route component

---
+++ client/src/components/routes/PrivateRoute.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const authContext = useContext(AuthContext);
+  const { isAuthenticated, loading } = authContext;
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        !isAuthenticated && !loading ? (
+          <Redirect to='/login' />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
+  );
+};
+
+// below if using redux
+
+// PrivateRoute.propTypes = {
+//   auth: PropTypes.object.isRequired
+// };
+
+// const mapStateToProps = state => ({
+//   auth: state.auth
+// });
+
+// export default connect(mapStateToProps)(PrivateRoute);
+
+export default PrivateRoute;
