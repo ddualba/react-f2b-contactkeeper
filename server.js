fbb7eeeadcea418f50ffb8fee48d80273d428b26
Dar Dualba <ddualba@gmail.com>
2020-11-15 22:20:17 -0800

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,11 +11,19 @@ connectDB();
 // Init Middleware (Provides access to req.body)
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to ContactKepper API' }));
-
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
